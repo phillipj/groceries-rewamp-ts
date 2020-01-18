7f3ccd6ac63e1690d550f44d170372f4185f5e99
Phillip Johnsen <johphi@gmail.com>
2020-01-18 22:32:35 +0100

    Grouped related tests in describe() blocks for easier discoverbility

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -13,64 +13,66 @@ function submitGrocery(groceryText: string, labelToClick: Element) {
   fireEvent.submit(focusedElement)
 }
 
-test('text filled in and submitted should be rendered as a grocery afterwards', () => {
-  const { getByText, getByLabelText } = render(<App />)
+describe('adding groceries', () => {
+  test('text filled in and submitted should be rendered as a grocery afterwards', () => {
+    const { getByText, getByLabelText } = render(<App />)
 
-  submitGrocery('Gulrot', getByLabelText('Ny matvare..'))
+    submitGrocery('Gulrot', getByLabelText('Ny matvare..'))
 
-  expect(getByText('Gulrot')).toBeInTheDocument()
-})
-
-test('submitting the same grocery several times should only result in one grocery rendered', () => {
-  const { getAllByText, getByLabelText } = render(<App />)
-
-  submitGrocery('Gulrot', getByLabelText('Ny matvare..'))
-  submitGrocery('Gulrot', getByLabelText('Ny matvare..'))
-
-  expect(getAllByText('Gulrot')).toHaveLength(1)
-})
-
-test('submitting the same grocery only different by whitespace should result in only one grocery rendered', () => {
-  const { queryAllByText, getByLabelText } = render(<App />)
-
-  submitGrocery('Gulrot', getByLabelText('Ny matvare..'))
-  submitGrocery('Gulrot ', getByLabelText('Ny matvare..'))
-
-  expect(queryAllByText('Gulrot')).toHaveLength(1)
-})
-
-test('latest grocery added is rendered first in the list', () => {
-  const { getAllByTestId, getByLabelText } = render(<App />)
-
-  submitGrocery('Gulrot', getByLabelText('Ny matvare..'))
-  submitGrocery('Brokkoli ', getByLabelText('Ny matvare..'))
-
-  const groceries = getAllByTestId('grocery').map(grocery => grocery.textContent)
-  expect(groceries).toEqual(['Brokkoli', 'Gulrot'])
-})
-
-test('clicking a grocery to mark it as complete moves the grocery below all uncompleted groceries after a while', () => {
-  const { getAllByTestId, getByLabelText } = render(<App />)
-
-  submitGrocery('Gulrot', getByLabelText('Ny matvare..'))
-  submitGrocery('Brokkoli ', getByLabelText('Ny matvare..'))
-
-  const firstGrocery = getAllByTestId('grocery')[0]
-
-  fireEvent.click(firstGrocery)
-
-  // act() is needed whenever code gets executed that causes state changes, probably only if
-  // we're not using testing-library functions, since those functions should know for themselfs
-  // when state changes might occur
-  act(() => {
-    jest.runAllTimers()
+    expect(getByText('Gulrot')).toBeInTheDocument()
   })
 
-  const groceries = getAllByTestId('grocery').map(grocery => grocery.textContent)
-  expect(groceries).toEqual(['Gulrot', 'Brokkoli'])
+  test('submitting the same grocery several times should only result in one grocery rendered', () => {
+    const { getAllByText, getByLabelText } = render(<App />)
+
+    submitGrocery('Gulrot', getByLabelText('Ny matvare..'))
+    submitGrocery('Gulrot', getByLabelText('Ny matvare..'))
+
+    expect(getAllByText('Gulrot')).toHaveLength(1)
+  })
+
+  test('submitting the same grocery only different by whitespace should result in only one grocery rendered', () => {
+    const { queryAllByText, getByLabelText } = render(<App />)
+
+    submitGrocery('Gulrot', getByLabelText('Ny matvare..'))
+    submitGrocery('Gulrot ', getByLabelText('Ny matvare..'))
+
+    expect(queryAllByText('Gulrot')).toHaveLength(1)
+  })
 })
 
+describe('sorting', () => {
+  test('latest grocery added is rendered first in the list', () => {
+    const { getAllByTestId, getByLabelText } = render(<App />)
 
+    submitGrocery('Gulrot', getByLabelText('Ny matvare..'))
+    submitGrocery('Brokkoli ', getByLabelText('Ny matvare..'))
+
+    const groceries = getAllByTestId('grocery').map(grocery => grocery.textContent)
+    expect(groceries).toEqual(['Brokkoli', 'Gulrot'])
+  })
+
+  test('clicking a grocery to mark it as complete moves the grocery below all uncompleted groceries after a while', () => {
+    const { getAllByTestId, getByLabelText } = render(<App />)
+
+    submitGrocery('Gulrot', getByLabelText('Ny matvare..'))
+    submitGrocery('Brokkoli ', getByLabelText('Ny matvare..'))
+
+    const firstGrocery = getAllByTestId('grocery')[0]
+
+    fireEvent.click(firstGrocery)
+
+    // act() is needed whenever code gets executed that causes state changes, probably only if
+    // we're not using testing-library functions, since those functions should know for themselfs
+    // when state changes might occur
+    act(() => {
+      jest.runAllTimers()
+    })
+
+    const groceries = getAllByTestId('grocery').map(grocery => grocery.textContent)
+    expect(groceries).toEqual(['Gulrot', 'Brokkoli'])
+  })
+})
 
 describe('deleting groceries', () => {
   test('long pressing a grocery should make delete button appear after a while', () => {
